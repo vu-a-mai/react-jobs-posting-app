3025d0194b3129eec0d34a3cea9aaaa326972dc9
Vu Mai <vuanhmai.io@gmail.com>
2024-04-16 02:35:29 -0700

    -Initial Commit -Initial Vite.config.js set localhost to port 3000 for react -React Jobs Posting Project -Initial Tailwind CSS Setup -Initial HomePage Setup -Implemented Components -Read Jobs Listing from .json file -Installed React-Icons

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <HomeCards />
+    <JobListings />
+    <ViewAllJobs />
+    </>
+  )
+}
+
+export default App
