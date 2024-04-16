d57993f4a4b0ff443fbd793e77b6e2a770235d63
Vu Mai <vuanhmai.io@gmail.com>
2024-04-16 15:12:29 -0700

    - Implemented React Router - Implemented MainLayout - Implemented HomePage and move previous components from index to HomePage - Implement Link and NavLink from React DOM: replacing all <a href> tag to <Link to> tag - Implmented Custom 404's Not Found Page - Updated NavBar component with active links with NavLink - Implemented JobsPage to display JobListings - Updated JobListings component to have conditioning state to pass in isHome prop for conditioning rendering "Recent Jobs":"Browse Jobs" - Fixed components and pages code formatting to make it look cleaner

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
 import { FaMapMarker } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
-const JobListing = ({job}) => {
+const JobListing = ({ job }) => {
     const [showFullDescription, setShowFullDescription] = useState(false);
 
     let description = job.description;
-    if(!showFullDescription){
+    if (!showFullDescription) {
         description = description.substring(0, 100) + '...';
     }
     return (
@@ -33,12 +34,11 @@ const JobListing = ({job}) => {
                             <FaMapMarker className='inline text-lg mb-1 mr-1' />
                             {job.location}
                         </div>
-                        <a
-                            href={`/job/${job.id}`}
-                            className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                        >
+                        <Link
+                            to={`/job/${job.id}`}
+                            className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
                             Read More
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
