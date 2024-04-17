dcf2560bb6dca4b9153d4390ef96f4c121f057a8
Vu Mai <vuanhmai.io@gmail.com>
2024-04-17 04:09:33 -0700

    - Implemented AddJobPage with Form Submission - Form Submission: on submitForm, take in prop addJobSubmit and addJob which use method POST add insert newJob data json

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
-import { 
+import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
-  RouterProvider 
+  RouterProvider
 } from 'react-router-dom'
 import React from 'react'
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
-import JobPage, {jobLoader} from './pages/JobPage'
+import JobPage, { jobLoader } from './pages/JobPage'
+import AddJobPage from './pages/AddJobPage'
 
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}> 
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Route>
-  )
-)
 const App = () => {
+
+  const addJob = async (newJob) => {
+    //console.log(newJob);
+    const response = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+  };
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    )
+  )
+
   return <RouterProvider router={router} />;
 }
 
