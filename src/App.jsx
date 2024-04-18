eb11630dbab77fb510257145b4252268ee623549
Vu Mai <vuanhmai.io@gmail.com>
2024-04-17 20:17:51 -0700

    - Implemented Edit Job Page to handle Job Listing update - Updated URL for Edit Job from  "/jobs/edit{$job.id}" to "/edit-job/${job.id}"

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage, { jobLoader } from './pages/JobPage'
 import AddJobPage from './pages/AddJobPage'
+import EditJobPage from './pages/EditJobPage'
 
 
 const App = () => {
@@ -29,7 +30,7 @@ const App = () => {
 
   // Delete Job
   const deleteJob = async (id) => {
-    console.log('delete', id);
+    // console.log('delete', id);
 
     const response = await fetch(`/api/jobs/${id}`, {
       method: 'DELETE',
@@ -41,13 +42,26 @@ const App = () => {
     return;
   };
 
+  // Update Job
+  const updateJob = async (job) => {
+    const response = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    });
+    return;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path='/jobs/:id' element={<JobPage  deleteJob={deleteJob}/>} loader={jobLoader} />
+        <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     )
