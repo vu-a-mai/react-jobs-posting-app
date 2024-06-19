351fa10ea23da133e31cfe0e5958d1e7e9f45e34
Vu Mai <vuanhmai.io@gmail.com>
2024-06-19 04:12:15 -0700

    update target to use json server host on vercel instead of localhost

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://json-server-vercel-ten-liard.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
